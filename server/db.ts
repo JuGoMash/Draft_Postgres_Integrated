35888249f0985156ee5ce3c0dfe90e32d6c6eeb1
JuGoMash <69407056+JuGoMash@users.noreply.github.com>
2025-07-20 10:16:28 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,12 @@
+
+import pkg from 'pg';
+export const { Pool } = pkg;
+
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
 import * as schema from './schema';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle(pool, { schema });
