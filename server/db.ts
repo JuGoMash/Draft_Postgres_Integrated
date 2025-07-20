0d20521242f06b9fa52890d977c8fdd26c953843
JuGoMash <69407056+JuGoMash@users.noreply.github.com>
2025-07-20 10:02:39 +0200

    server

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,9 @@
-
-import { Pool } from "pg";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
-export default pool;
+export const db = drizzle(pool, { schema });
