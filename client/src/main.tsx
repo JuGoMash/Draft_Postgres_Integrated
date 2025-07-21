6bd013caaaa2aee201ccffa9e97d557fbef42b34
JuGoMash <69407056+JuGoMash@users.noreply.github.com>
2025-07-21 11:53:55 +0200

    Updated UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const App = () => <h1>Hello Vite + React!</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
